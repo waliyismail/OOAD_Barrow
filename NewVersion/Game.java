5c3d4c76f82314c652353283a0d056d4b2234ff7
nrhing <70261743+nrhing@users.noreply.github.com>
2020-09-20 11:28:48 +0800

    Update Game.java

--- NewVersion/Game.java
+++ NewVersion/Game.java
@@ -16,17 +16,17 @@ public class Game {
 		Board gameBoard = new Board();
 	
 		//red pieces
-		ChessPiece redSun = new Sun("sun", "red", 7, 3);
-		Chevron redChevron1 = new Chevron("chevron", "red", 7, 2);
-		Chevron redChevron2 = new Chevron("chevron", "red", 7, 4);
-		Triangle redTriangle1 = new Triangle("triangle", "red", 7, 1);
-		Triangle redTriangle2 = new Triangle("triangle", "red", 7, 5);
-		Plus redPlus1 = new Plus("plus", "red", 7, 0);
-		Plus redPlus2 = new Plus("plus", "red", 7, 6);
-		Arrow redArrow1 = new Arrow("Arrow","red", 6, 0, "up");
-		Arrow redArrow2 = new Arrow("Arrow","red", 6, 2,"up");
-		Arrow redArrow3 = new Arrow("Arrow","red", 6, 4,"up");
-		Arrow redArrow4 = new Arrow("Arrow","red", 6, 6, "up");
+		ChessPiece redSun = new Sun("sun", "red", 0, 3);
+		Chevron redChevron1 = new Chevron("chevron", "red", 0, 2);
+		Chevron redChevron2 = new Chevron("chevron", "red", 0, 4);
+		Triangle redTriangle1 = new Triangle("triangle", "red", 0, 1);
+		Triangle redTriangle2 = new Triangle("triangle", "red", 0, 5);
+		Plus redPlus1 = new Plus("plus", "red", 0, 0);
+		Plus redPlus2 = new Plus("plus", "red", 0, 6);
+		Arrow redArrow1 = new Arrow("Arrow","red", 6, 0, "down");
+		Arrow redArrow2 = new Arrow("Arrow","red", 6, 2,"down");
+		Arrow redArrow3 = new Arrow("Arrow","red", 6, 4,"down");
+		Arrow redArrow4 = new Arrow("Arrow","red", 6, 6, "down");
 		gameBoard.pieceSetup(redSun);
 		gameBoard.pieceSetup(redChevron1);
 		gameBoard.pieceSetup(redChevron2);
@@ -34,10 +34,10 @@ public class Game {
 		gameBoard.pieceSetup(redTriangle2);
 		gameBoard.pieceSetup(redPlus1);
 		gameBoard.pieceSetup(redPlus2);
-		gameBoard.pieceSetup(redArrow1);
-		gameBoard.pieceSetup(redArrow2);
-		gameBoard.pieceSetup(redArrow3);
-		gameBoard.pieceSetup(redArrow4);
+		gameBoard.pieceSetupArrows(redArrow1);
+		gameBoard.pieceSetupArrows(redArrow2);
+		gameBoard.pieceSetupArrows(redArrow3);
+		gameBoard.pieceSetupArrows(redArrow4);
 		
 		//blue pieces
 		Sun blueSun = new Sun("sun", "blue", 7, 3);
@@ -47,10 +47,10 @@ public class Game {
 		Triangle blueTriangle2 = new Triangle("triangle", "blue", 7, 5);
 		Plus bluePlus1 = new Plus("plus", "blue", 7, 0);
 		Plus bluePlus2 = new Plus("plus", "blue", 7, 6);
-		Arrow blueArrow1 = new Arrow("Arrow","blue", 6, 0, "down");
-		Arrow blueArrow2 = new Arrow("Arrow","blue", 6, 2, "down");
-		Arrow blueArrow3 = new Arrow("Arrow","blue", 6, 4, "down");
-		Arrow blueArrow4 = new Arrow("Arrow","blue", 6, 6, "down");
+		Arrow blueArrow1 = new Arrow("Arrow","blue", 6, 0, "up");
+		Arrow blueArrow2 = new Arrow("Arrow","blue", 6, 2, "up");
+		Arrow blueArrow3 = new Arrow("Arrow","blue", 6, 4, "up");
+		Arrow blueArrow4 = new Arrow("Arrow","blue", 6, 6, "up");
 		gameBoard.pieceSetup(blueSun);
 		gameBoard.pieceSetup(blueChevron1);
 		gameBoard.pieceSetup(blueChevron2);
@@ -58,10 +58,10 @@ public class Game {
 		gameBoard.pieceSetup(blueTriangle2);
 		gameBoard.pieceSetup(bluePlus1);
 		gameBoard.pieceSetup(bluePlus2);
-		gameBoard.pieceSetup(blueArrow1);
-		gameBoard.pieceSetup(blueArrow2);
-		gameBoard.pieceSetup(blueArrow3);
-		gameBoard.pieceSetup(blueArrow4);
+		gameBoard.pieceSetupArrows(blueArrow1);
+		gameBoard.pieceSetupArrows(blueArrow2);
+		gameBoard.pieceSetupArrows(blueArrow3);
+		gameBoard.pieceSetupArrows(blueArrow4);
 		pieces.add(redSun);
 		gameBoard.setPieces(pieces);
 
